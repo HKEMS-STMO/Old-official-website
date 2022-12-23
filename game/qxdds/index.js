128efc82f85b1b02fe5a28d9a15e05f36de72baa
HKEMS-STMO_MQSI <118874898+X-MQSI@users.noreply.github.com>
2022-12-23 21:54:28 +0800

    Update index.js

--- game/qxdds/index.js
+++ game/qxdds/index.js
@@ -5,22 +5,22 @@ for(var i=1;i<17;i++){
 $('section div').append("<img class='hole' src='image/hole.png'  >");
 $('section div').append('<img class="mouse" src="image/mouse.png">');
 
-// 设置初始状态地鼠都没出现
+// 设置初始状态千学都没出现
 
  $('.mouse').hide();
 
-//袋鼠出现
+//千学出现
                    // [1,4] [0, 3)
-// 随机出现的袋鼠数目 [1 16]  (0, 15]  向上取整
+// 随机出现的千学数目 [1 16]  (0, 15]  向上取整
 var num
 // 
-//袋鼠位置[0  15]  索引 index
+//千学位置[0  15]  索引 index
  var index
  var object;
 setInterval(function(){
     num=Math.ceil(Math.random()*2);
     for(var i=1; i<=num ;i++){
-        //随机出现的袋鼠位置[0  15]  索引
+        //随机出现的千学位置[0  15]  索引
        index=Math.floor(Math.random()*16);
       object=$('section div:nth-of-type('+(index+1)+') .mouse')
    
@@ -31,7 +31,7 @@ setInterval(function(){
 },1000);
 
 
-// score 记录敲到地鼠的分数
+// score 记录敲到千学的分数
 var score=0;
 $('.mouse').click(function(){
    
