047581dc7df443a9644310e5055abaa3bd9b2f09
HKEMS-STMO_MQSI <118874898+X-MQSI@users.noreply.github.com>
2023-02-17 07:19:06 +0800

    数据更新

--- js/end.js
+++ js/end.js
@@ -1,1 +1,4 @@
-end.innerHTML = "Copyright 1998-2023 HKEMS-STMO All rights reserved.海口实验中学科技社 版权所有"
+window.onload = function() {
+    var end = document.getElementById("end");
+    end.innerHTML="<p>Copyright 1998-2023 HKEMS-STMO All rights reserved.</p><p>海口实验中学科技社 版权所有</p><p><a href='/MQSI/index.html' target='_blank'>技术支持:WEB By MQSI</a></p><p><span id='span'></span></p>";
+  }
