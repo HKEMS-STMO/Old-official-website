5c7885a243211f13af5c7fad1604cb9a32ba40e2
HKEMS-STMO_MQSI <118874898+X-MQSI@users.noreply.github.com>
2023-02-17 16:56:21 +0800

    数据更新

---
+++ js/访问计数.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    var fangwenjishu = document.getElementById("fangwenjishu");
+    fangwenjishu.innerHTML="<span id='busuanzi_container_site_pv' class='auto-style19'>&nbsp;&nbsp;&nbsp; 欢迎您的光临！本站已被访问</span><span id='busuanzi_value_site_pv'>114514</span><span>次</span>";
+  }
