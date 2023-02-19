cfdaf1b87062cc9c465d32535c62acc55ec62765
HKEMS-STMO_MQSI <maqingshui@outlook.com>
2023-02-19 19:34:11 +0800

    数据更新

--- js/Click on special effects.js
+++ js/Click on special effects.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("厚德","博学","明志","和谐","探索","进取","勤思","开拓","合作","细致","创新");
+        var a = new Array("厚德","博学","明志","和谐","探索","进取","勤思","创新");
         var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
--- js/end.js
+++ js/end.js
@@ -1,4 +1,4 @@
 window.onload = function() {
     var end = document.getElementById("end");
-    end.innerHTML="<p>Copyright 1998-2023 HKEMS-STMO All rights reserved.</p><p>海口实验中学科技社 版权所有</p><p><a href='/MQSI/index.html' target='_blank'>技术支持:WEB By MQSI</a></p><p><span id='span'></span></p>";
+    end.innerHTML="<p>我们遵循&nbsp;<a href='https://creativecommons.org/publicdomain/zero/1.0/deed.zh'>CC0 1.0 通用 (CC0 1.0)公共领域贡献</a>&nbsp;开源协议</p><p>Create By HKEMS-STMO (1998-2023)</p><p>本站由 海口实验中学科技社 创建</p><p><a href='/MQSI/index.html' target='_blank'>技术支持:WEB By MQSI</a></p><p><span id='span'></span></p>";
   }
